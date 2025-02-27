6e3a9532ad01cbe67c1972aaf6f7a5870b061fa7
ShadowTryDragon <90843434+ShadowTryDragon@users.noreply.github.com>
2025-02-27 13:15:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-
 import os
 import discord
+from discord import client
 from dotenv import load_dotenv
+
+
+
 
 intents = discord.Intents.default()
 intents.members = True
+
 
 status = discord.Status.dnd
 activity = discord.Activity(type=discord.ActivityType.playing, name="Supporter")
@@ -12,15 +16,13 @@ activity = discord.Activity(type=discord.ActivityType.playing, name="Supporter")
 bot = discord.Bot(
     intents=intents,
     debug_guilds=[523109595292368907],
-    status = status,
-    activity = activity
+    status=status,
+    activity=activity
 )
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} ist Online")
-
-
 
 if __name__ == "__main__":
     for filename in os.listdir("cogs"):
@@ -28,8 +30,11 @@ if __name__ == "__main__":
             bot.load_extension(f"cogs.{filename[:-3]}")
 
 
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--token", required=True, help="Discord Bot Token")
+args = parser.parse_args()
 
-
-load_dotenv()
-bot.run("MTM0NDM0MTU0NDIzMjAzMDI0OQ.GlIerP.RWgX6yiSkdfVVihJUtMGhh9349yXpTUh8l9ydY")
+TOKEN = args.token
+bot.run(TOKEN)
